24cd7e142844a4a9083ffe65a4c4729b48f9556b
alu0100821390 <alu0100821390@ull.edu.es>
2017-04-01 21:06:01 +0000

    Añadir pruebas para las clases TextCell, UnderlinedCell y RTextCell

--- test/test.js
+++ test/test.js
@@ -1,7 +1,11 @@
-// Testing drawIt
 var dt = require("../lyingoutatable.js");
 var drawIt = dt.drawIt;
+var drawTable = dt.drawTable;
+var TextCell = dt.TextCell;
+var UnderlinedCell = dt.UnderlinedCell;
+var RTextCell = dt.RTextCell;
 
+// Testing drawIt
 var MOUNTAINS = [
   {name: "Kilimanjaro\nMontaña mágica", height: 5895, country: "Tanzania"},
   {name: "Everest", height: 8848, country: "Nepal\nPaís lejano"},
@@ -26,14 +30,12 @@ Denali           6168 United States
 Popocatepetl     5465 Mexico       `;
 
 describe("drawIt", function() {
-  it("must draw the mountains table correctly", function() {
+  it("Must draw the mountains table correctly", function() {
     drawIt(MOUNTAINS).should.equal(expected);
   })
 });
 
-var TextCell = dt.TextCell;
-var drawTable = dt.drawTable;
-
+// Testing drawTable
 function checkerboard() {
   var rows = [];
   for (var i = 0; i < 5; i++) {
@@ -52,8 +54,44 @@ var expectedCheckerboard =
 ##    ##    ##
    ##    ##   
 ##    ##    ##`;
+
 describe("drawTable", function() {
-  it("must draw the checkerboard correctly", function() {
+  it("Must draw the checkerboard correctly", function() {
     drawTable(checkerboard()).should.equal(expectedCheckerboard);
+  })
+});
+
+// Testing TextCell
+var TCell = new TextCell(`Kilimanjaro\nMontaña mágica`);
+
+var expectedTextCell = [ `Kilimanjaro   `, `Montaña mágica` ];
+
+describe("TextCell", function() {
+  it("Must draw the TextCell correctly", function() {
+    (TCell.draw(14, 2)[0]).should.equal(expectedTextCell[0]);
+    (TCell.draw(14, 2)[1]).should.equal(expectedTextCell[1]);
+  })
+});
+
+// Testing UnderlinedCell
+var UCell = new UnderlinedCell(new TextCell(`Everest`));
+
+var expectedUnderlinedCell = [`Everest       `, `--------------`];
+
+describe("UnderlinedCell", function() {
+  it("Must draw the UnderlinedCell correctly", function() {
+    (UCell.draw(14, 2)[0]).should.equal(expectedUnderlinedCell[0]);
+    (UCell.draw(14, 2)[1]).should.equal(expectedUnderlinedCell[1]);
+  })
+});
+
+// Testing RTextCell
+var RTCell = new RTextCell(`5895`);
+
+var expectedRTextCell = [`          5895`];
+
+describe("RTextCell", function() {
+  it("Must draw the RTextCell correctly", function() {
+    (RTCell.draw(14, 1)[0]).should.equal(expectedRTextCell[0]);
   })
 });
